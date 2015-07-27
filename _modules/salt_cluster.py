f5520c977ff70159d674cdfd239f379ff1c3f023
Justin Findlay <jfindlay@gmail.com>
2015-07-26 20:41:09 -0600

    adding salt ssh cluster exec and state mods

---
+++ _modules/salt_cluster.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+'''
+
+'''
+from __future__ import absolute_import
+
+# Import python libs
+import os
+import yaml
+import logging
+import textwrap
+
+# Import salt libs
+from salt.exceptions import CommandExecutionError
+
+
+log = logging.getLogger(__name__)
+__outputter__ = {
+    'create_node': 'highstate',
+    'destroy_node': 'highstate',
+}
+
+
+def _cmd(*args):
+    '''
+    construct salt-cloud command
+    '''
+    cmd = ['salt-cloud', '--output=yaml', '--assume-yes']
+    cmd.extend(args)
+    return cmd
+
+
+def _get_passwd(profile, prof_dir='/etc/salt/cloud.profiles.d'):
+    '''
+    retrieve profile password from profile config
+    '''
+    for prof_file_name in os.listdir(prof_dir):
+        with open(os.path.join(prof_dir, prof_file_name)) as prof_file:
+            prof_data = yaml.load(prof_file.read())
+            if profile in prof_data:
+                return prof_data[profile]['password']
+
+    error = 'Failed to retrieve password for {0} from cloud profiles, {1}'.format(profile, prof_dir)
+    log.error(error)
+    raise CommandExecutionError(error)
+
+
+def _add_to_roster(name, host, user, passwd, roster):
+    '''
+    add a cloud instance to the cluster roster
+    '''
+    entry = textwrap.dedent('''\
+        {0}:
+          host: {1}
+          user: {2}
+          passwd: {3}'''.format(name, host, user, passwd))
+    __salt__['state.single']('file.touch', roster, makedirs=True)
+    __salt__['file.blockreplace'](roster,
+             '# begin {0}'.format(name),
+             '# end {0}'.format(name),
+             entry,
+             append_if_not_found=True)
+
+
+def _rem_from_roster(name, roster):
+    '''
+    remove a cloud instance from the cluster roster
+    '''
+    # remove config block
+    __salt__['file.blockreplace'](roster,
+                                  '# begin {0}'.format(name),
+                                  '# end {0}'.format(name))
+
+    # remove block markers
+    __salt__['file.replace'](roster,
+                             r'^# begin {0}$\n'.format(name),
+                             '')
+    __salt__['file.replace'](roster,
+                             r'^# end {0}$\n'.format(name),
+                             '')
+
+
+def create_node(name, profile, user='root', roster='/etc/salt/cluster/roster'):
+    '''
+    Create a cloud instance using salt-cloud and add it to the cluster roster
+
+    .. code-block:: bash
+
+        salt master-minion salt_cluster.create_node jmoney-master linode-centos-7 root /tmp/roster
+    '''
+    args = ['--no-deploy', '--profile', profile, name]
+    info = yaml.load(__salt__['cmd.run_stdout'](_cmd(*args)))
+
+    if name in info and info[name].get('state') == 'Running':
+        passwd = _get_passwd(profile)
+        _add_to_roster(name, info[name]['public_ips'][0], user, passwd, roster)
+        return True
+
+    else:
+        error = 'Failed to create node {0} from profile {1}: {2}'.format(name, profile, info)
+        log.error(error)
+        return (False, error)
+
+
+def destroy_node(name, roster='/etc/salt/cluster/roster'):
+    '''
+    Destroy a cloud instance using salt-cloud and remove it from the cluster roster
+
+    .. code-block:: bash
+
+        salt master-minion salt_cluster.destroy_node jmoney-master
+    '''
+    args = ['--destroy', name]
+    info = yaml.load(__salt__['cmd.run_stdout'](_cmd(*args)))
+    if isinstance(info, dict) and name in str(info):
+        _rem_from_roster(name, roster)
+        return True
+    else:
+        error = 'Failed to remove node {0}: {1}'.format(name, info)
+        log.error(error)
+        return (False, error)
---
+++ _states/salt_cluster.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+'''
+Create and destroy clusters of VMs.
+
+.. code-block:: yaml
+
+    master-and-minion:
+      salt_cluster.present:
+        - linode-centos-7:
+            jmoney-master
+        - linode-ubuntu-15:
+            jmoney-minion
+
+.. code-block:: yaml
+
+    master-syndic-minions:
+      salt_cluster.present:
+        - linode-centos-7:
+            jmoney-master
+            jmoney-syndic
+        - rackspace-gentoo:
+            jmoney-gentoo-minion
+        - vultr-debian-8:
+            jmoney-debian-minion
+
+.. code-block:: yaml
+
+    delete-cluster:
+      salt_cluster.absent:
+        - jmoney-master
+        - jmoney-minion
+'''
+
+# Import salt libs
+from salt.exceptions import SaltInvocationError
+
+
+__outputter__ = {
+    'present': 'highstate',
+    'absent': 'highstate',
+}
+
+
+def node_present(name, profile):
+    '''
+    Create a salt cluster node
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'result': False,
+           'comment': ''}
+
+    if __salt__['cloud.has_instance'](name):
+        ret['result'] = True
+        ret['comment'] = 'Cluster node {0} already present'.format(name)
+        return ret
+    if __opts__['test']:
+        ret['result'] = None
+        ret['comment'] = 'Cluster node {0} is set to be created'.format(name)
+        return ret
+
+    info = __salt__['salt_cluster.create_node'](name, profile)
+
+    if info is True:
+        ret['changes'] = {'node': name}
+        ret['result'] = True
+        ret['comment'] = 'Cluster node {0} created from cloud profile {1}'.format(name, profile)
+        return ret
+    else:
+        ret['result'] = False
+        ret['comment'] = info[1]
+        return ret
+
+
+def node_absent(name):
+    '''
+    Destroy a salt cluster node
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'result': False,
+           'comment': ''}
+
+    if not __salt__['cloud.has_instance'](name):
+        ret['result'] = True
+        ret['comment'] = 'Cluster node {0} already absent'.format(name)
+        return ret
+    if __opts__['test']:
+        ret['result'] = None
+        ret['comment'] = 'Cluster node {0} is set to be destroyed'.format(name)
+        return ret
+
+    info = __salt__['salt_cluster.destroy_node'](name)
+
+    if info is True:
+        ret['changes'] = {'node': name}
+        ret['result'] = True
+        ret['comment'] = 'Cluster node {0} destroyed'.format(name)
+        return ret
+    else:
+        ret['result'] = False
+        ret['comment'] = info[1]
+        return ret
+
+
+def present(name, profiles):
+    '''
+    Create a salt cluster with the nodes named under the profiles
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'result': None if __opts__['test'] else False,
+           'comment': ''}
+
+    node_rets = []
+    for prof_map in profiles:
+        profile = prof_map.keys()[0]
+        for node_name in prof_map[profile]:
+            node_ret = node_present(node_name, profile)
+            node_rets.append(node_ret)
+
+            # changes
+            if node_ret['changes']:
+                if not 'nodes' in ret['changes']:
+                    ret['changes']['nodes'] = []
+                ret['changes']['nodes'].append(node_name)
+
+            # result
+            ret['result'] = ret['result'] or node_ret['result']
+
+    # comment
+    if ret['result']:
+        if ret['changes']:
+            ret['comment'] = 'Cluster {0} created'.format(name)
+        else:
+            ret['comment'] = 'Cluster {0} already present'.format(name)
+    elif ret['result'] is None:
+        ret['comment'] = 'Cluster {0} is set to be created'.format(name)
+    else:
+        ret['comment'] = 'Cluster {0} failed to be created: {1}'.format(name, [nr['comment'] for nr in node_rets])
+
+    return ret
+
+
+def absent(name, profiles):
+    '''
+    Destroy a salt cluster with the nodes named under the profiles
+    '''
+    ret = {'name': name,
+           'changes': {},
+           'result': None if __opts__['test'] else False,
+           'comment': ''}
+
+    node_rets = []
+    for prof_map in profiles:
+        profile = prof_map.keys()[0]
+        for node_name in prof_map[profile]:
+            node_ret = node_absent(node_name)
+            node_rets.append(node_ret)
+
+            # changes
+            if node_ret['changes']:
+                if not 'nodes' in ret['changes']:
+                    ret['changes']['nodes'] = []
+                ret['changes']['nodes'].append(node_name)
+
+            # result
+            ret['result'] = ret['result'] or node_ret['result']
+
+    # comment
+    if ret['result']:
+        if ret['changes']:
+            ret['comment'] = 'Cluster {0} destroyed'.format(name)
+        else:
+            ret['comment'] = 'Cluster {0} already absent'.format(name)
+    elif ret['result'] is None:
+        ret['comment'] = 'Cluster {0} is set to be destroyed'.format(name)
+    else:
+        ret['comment'] = 'Cluster {0} failed to be destroyed: {1}'.format(name, [nr['comment'] for nr in node_rets])
+
+    return ret
